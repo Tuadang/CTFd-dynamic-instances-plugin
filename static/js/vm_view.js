7bcb17198d506a49f4a71c515f135047ec08c0f2
Tuadang <D.vanw05@gmail.com>
2026-01-22 10:17:54 +0100

    update k8s view to see if it works

--- static/js/vm_view.js
+++ static/js/vm_view.js
@@ -1,69 +1,119 @@
-CTFd._internal.challenge = {
-    data: {},
-
-    preRender: function () {
-        // Called before the modal renders
-        return Promise.resolve();
-    },
-
-    render: function (challenge) {
-        // Save challenge data for later use
-        CTFd._internal.challenge.data = challenge;
-        return Promise.resolve();
-    },
-
-    postRender: function () {
-        // Attach your custom button handlers
-        const startBtn = document.getElementById("start-instance");
-        const stopBtn = document.getElementById("stop-instance");
-        const statusBtn = document.getElementById("status-instance");
-
-        if (startBtn) startBtn.onclick = CTFd._internal.challenge.startInstance;
-        if (stopBtn) stopBtn.onclick = CTFd._internal.challenge.stopInstance;
-        if (statusBtn) statusBtn.onclick = CTFd._internal.challenge.statusInstance;
-
-        return Promise.resolve();
-    },
-
-    startInstance: function () {
-        const id = CTFd._internal.challenge.data.id;
-
-        fetch(`/plugins/dynamic_instances/${id}/start`, {
-            method: "POST",
-            credentials: "same-origin"
-        })
-        .then(r => r.json())
-        .then(data => {
-            document.getElementById("instance-info").textContent =
-                JSON.stringify(data, null, 2);
-        });
-    },
-
-    stopInstance: function () {
-        const id = CTFd._internal.challenge.data.id;
-
-        fetch(`/plugins/dynamic_instances/${id}/stop`, {
-            method: "POST",
-            credentials: "same-origin"
-        })
-        .then(r => r.json())
-        .then(data => {
-            document.getElementById("instance-info").textContent =
-                JSON.stringify(data, null, 2);
-        });
-    },
-
-    statusInstance: function () {
-        const id = CTFd._internal.challenge.data.id;
-
-        fetch(`/plugins/dynamic_instances/${id}/status`, {
-            method: "GET",
-            credentials: "same-origin"
-        })
-        .then(r => r.json())
-        .then(data => {
-            document.getElementById("instance-info").textContent =
-                JSON.stringify(data, null, 2);
-        });
+(function () {
+    // Ensure CTFd challenge system exists
+    if (!CTFd || !CTFd._internal || !CTFd._internal.challenge) {
+        console.error("CTFd challenge system not found");
+        return;
     }
-};
+
+    // Extend the existing challenge handler (DO NOT overwrite)
+    CTFd._internal.challenge = {
+        ...CTFd._internal.challenge,
+
+        data: {},
+
+        /**
+         * Called before the modal is rendered
+         */
+        preRender: function () {
+            return Promise.resolve();
+        },
+
+        /**
+         * Called with challenge data from API
+         */
+        render: function (challenge) {
+            this.data = challenge;
+            return Promise.resolve();
+        },
+
+        /**
+         * Called after modal HTML is in the DOM
+         */
+        postRender: function () {
+            const bind = (id, handler) => {
+                const el = document.getElementById(id);
+                if (el) {
+                    el.addEventListener("click", handler.bind(this));
+                }
+            };
+
+            bind("start-instance", this.startInstance);
+            bind("stop-instance", this.stopInstance);
+            bind("status-instance", this.statusInstance);
+
+            return Promise.resolve();
+        },
+
+        /**
+         * Start a per-user instance
+         */
+        startInstance: function () {
+            if (!this.data?.id) return;
+
+            fetch(`/plugins/dynamic_instances/${this.data.id}/start`, {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(r => r.json())
+                .then(data => {
+                    const info = document.getElementById("instance-info");
+                    if (info) {
+                        info.textContent = JSON.stringify(data, null, 2);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        },
+
+        /**
+         * Stop the user instance
+         */
+        stopInstance: function () {
+            if (!this.data?.id) return;
+
+            fetch(`/plugins/dynamic_instances/${this.data.id}/stop`, {
+                method: "POST",
+                credentials: "same-origin",
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(r => r.json())
+                .then(data => {
+                    const info = document.getElementById("instance-info");
+                    if (info) {
+                        info.textContent = JSON.stringify(data, null, 2);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        },
+
+        /**
+         * Get instance status
+         */
+        statusInstance: function () {
+            if (!this.data?.id) return;
+
+            fetch(`/plugins/dynamic_instances/${this.data.id}/status`, {
+                method: "GET",
+                credentials: "same-origin"
+            })
+                .then(r => r.json())
+                .then(data => {
+                    const info = document.getElementById("instance-info");
+                    if (info) {
+                        info.textContent = JSON.stringify(data, null, 2);
+                    }
+                })
+                .catch(err => {
+                    console.error(err);
+                });
+        }
+    };
+})();
