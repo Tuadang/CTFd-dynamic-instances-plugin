9a244f5d57c7405f27460f6f3083560347455b23
Tuadang <D.vanw05@gmail.com>
2026-01-27 10:12:48 +0100

    Updated api url

--- static/js/dynamic_instances_config.js
+++ static/js/dynamic_instances_config.js
@@ -1,3 +1,3 @@
 // Set the external provisioner API base before k8s/vm views run.
 // Update this value to your provisioner API URL (no trailing slash).
-window.DYNAMIC_INSTANCES_API_BASE = window.DYNAMIC_INSTANCES_API_BASE || "http://172.16.0.132:5000";
+window.DYNAMIC_INSTANCES_API_BASE = "http://172.16.0.132:5000";
